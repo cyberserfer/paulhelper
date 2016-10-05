d503cca62597de45a58a8a56988d8028a20a898c
Cary Wolff <boss@caryyon.com>
2016-10-05 18:46:00 -0500

    Just the first real commit with stuff

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// YO PAUL!!!
+// so if you are reading this you have properly cloned this repository!!!!
+
+// I don't know what kind of stuff you'd like to learn.
+// I'm gonna add just some basic libraries to this repo.
+
+// you can find all the extra libraries with in the node_modlues folder.
